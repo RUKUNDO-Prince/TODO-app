dc307f6f7183cc150985260bede344df06e0743c
RUKUNDO Prince <rukundoprince951@gmail.com>
2024-10-27 14:29:53 +0300

    modified some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#f86f03',
+        primary: '#4E4BBF',
+        secondary: '#B7F28D',
+        yellow: '#F2C063',
+        orange: '#F27405',
+        gray: '#F2F2F2',
+        purple: '#7C27F2',
       },
       animation: {
         fadeIn: 'fadeIn 0.5s ease-in-out',
